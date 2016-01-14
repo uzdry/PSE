ecc364128b46ffa41566621023045474b1f4af29
Valentin Springsklee <valentin@Valentins-MacBook-Pro.local>
2016-01-14 17:04:48 +0100

    Bus implement early stage

---
+++ VINJAB/Bus/Source.ts
@@ -0,0 +1,12 @@
+
+import BusDevice from "./Bus";
+/**
+ * Created by valentin on 12/01/16.
+ */
+
+
+export default class Source extends BusDevice {
+    constructor() {
+        super();
+    }
+}
