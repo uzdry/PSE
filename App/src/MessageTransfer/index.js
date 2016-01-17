0c6db5c1b7037026509f2d444f92a708b6a75e49
Yimeng Zhu <yimeng.zhu0821@gmail.com>
2016-01-17 18:00:42 +0100

    modified JSON

--- App/src/MessageTransfer/index.js
+++ App/src/MessageTransfer/index.js
@@ -15,14 +15,13 @@ io.on('connection', function(socket){
         console.log('user disconnected');
     });
     socket.on('chat message', function(msg){
-        console.log('send a message with the message ID ' + msg );
+        console.log('The Client is asking for the message with ID ' + msg );
 
         //processing the message and get the message value in the database
 
         io.emit('chat message', "the asked message (" + msg  + ") is blablabla.");
     });
 });
-
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
